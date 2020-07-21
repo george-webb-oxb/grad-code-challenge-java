5a8b33d464982cfee03484b053fbb051edb241ae
George Webb <george.webb@oxbotica.com>
2020-07-21 10:20:17 +0100

    Fix Comments

--- main.java
+++ main.java
@@ -18,9 +18,9 @@
 
 class Main {
   // 1. Write a function that takes in an array of unordered numbers
-  //    (unordered_numbers.h) and returns an array of ordered numbers 
+  //    (data.java) and returns an array of ordered numbers 
   //    (ascending order) using a merge sort. 
-  //    Don't use an in build sort function (that makes it too easy)
+  //    Don't use an sort function from java (that makes it too easy)
   // Merge Sort: https://www.bbc.co.uk/bitesize/guides/zjdkw6f/revision/5
 
   // 2. Write a function to show your sorting works on your ordered numbers
@@ -29,7 +29,7 @@ class Main {
   // 3. Write a function that performs a binary search to return 
   //    the index of a given number (if it exists!).
   // Binary Search: https://www.bbc.co.uk/bitesize/guides/zjdkw6f/revision/3
-  // (Try not to read the sudo code at the end of the page....)
+  // (Try not to read the pseudocode at the end of the page....)
 
   // 4. Write a function to show your binary search works 
   //    (e.g. Prints "Passed" to the terminal on an expected result).
